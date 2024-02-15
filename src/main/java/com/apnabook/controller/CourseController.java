dd4a20934679f4d7e6a4c3dc3cd3ff8d18a82ed4
syeffigo <158276441+syeffigo@users.noreply.github.com>
2024-02-15 16:50:57 +0530

    Update CourseController.java

--- src/main/java/com/apnabook/controller/CourseController.java
+++ src/main/java/com/apnabook/controller/CourseController.java
@@ -23,28 +23,37 @@ import com.apnabook.service.CourseService;
 @RestController
 @RequestMapping("/course")
 public class CourseController {
-  
+
 	@Autowired
 	private CourseService courseService;
-	
+
 	@PostMapping("/create")
-	public ResponseEntity<?> createCourses(@Valid @RequestBody CourseDto courseDto){
-		return courseService.addCourse(courseDto);
-	}
-	
-	@PostMapping("/view")
-	public ResponseEntity<?> viewCourses(@Valid @RequestBody GetCourseDto getCourseDto){
-		Optional<List<CoursesCategory>> response = courseService.getCourse(getCourseDto);
-		
-		if(response.isEmpty()) {
-			return new ResponseEntity<>(new HashMap<>().put("Message", "Category does not exists"), HttpStatus.BAD_REQUEST);
+	public ResponseEntity<?> createCourses(@Valid @RequestBody CourseDto courseDto) {
+		boolean create = courseService.addCourse(courseDto);
+		HashMap<String, String> response = new HashMap<>();
+		if (create) {
+			response.put("Message", "Course added successfully");
+			return new ResponseEntity<>(response, HttpStatus.CREATED);
+		} else {
+			response.put("Message", "Some error occurred");
+			return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
 		}
-		
+	}
+
+	@PostMapping("/view")
+	public ResponseEntity<?> viewCourses(@Valid @RequestBody GetCourseDto getCourseDto) {
+		Optional<List<CoursesCategory>> response = courseService.getCourse(getCourseDto);
+
+		if (response.isEmpty()) {
+			return new ResponseEntity<>(new HashMap<>().put("Message", "Category does not exists"),
+					HttpStatus.BAD_REQUEST);
+		}
+
 		return new ResponseEntity<>(response.get(), HttpStatus.OK);
 	}
-	
+
 	@PostMapping("/view/{id}")
-	public ResponseEntity<?> viewCourseById(@Valid @PathVariable Long id, @RequestBody GetCourseDto getCourseDto){
+	public ResponseEntity<?> viewCourseById(@Valid @PathVariable Long id, @RequestBody GetCourseDto getCourseDto) {
 		return courseService.getCourseId(id, getCourseDto);
 	}
 }

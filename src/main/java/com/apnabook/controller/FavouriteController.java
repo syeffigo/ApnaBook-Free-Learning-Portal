726e1b149275fcc32d6dcebfa0811947da009546
syeffigo <158276441+syeffigo@users.noreply.github.com>
2024-02-15 16:51:39 +0530

    Update FavouriteController.java

--- src/main/java/com/apnabook/controller/FavouriteController.java
+++ src/main/java/com/apnabook/controller/FavouriteController.java
@@ -1,8 +1,11 @@
 package com.apnabook.controller;
 
+import java.util.HashMap;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,18 +18,35 @@ import com.apnabook.service.FavouriteService;
 @RestController
 @RequestMapping("/favourite")
 public class FavouriteController {
-   
+
 	@Autowired
 	private FavouriteService favouriteService;
-	
+
 	@PostMapping("/make")
-	public ResponseEntity<?> makeFavourite(@Valid @RequestBody FavouriteDto favouriteDto){
-		return favouriteService.makeFavourite(favouriteDto);
+	public ResponseEntity<?> makeFavourite(@Valid @RequestBody FavouriteDto favouriteDto) {
+		boolean favourite = favouriteService.makeFavourite(favouriteDto);
+		HashMap<String, String> response = new HashMap<>();
+
+		if (favourite) {
+			response.put("Message", "Course added to favourites.");
+			return new ResponseEntity<>(response, HttpStatus.OK);
+		} else {
+			response.put("Message", "Validation error");
+			return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+		}
 	}
-	
+
 	@PostMapping("/remove")
-	public ResponseEntity<?> removeFavourite(@Valid @RequestBody FavouriteDto favouriteDto){
-		return favouriteService.removeFavourite(favouriteDto);
+	public ResponseEntity<?> removeFavourite(@Valid @RequestBody FavouriteDto favouriteDto) {
+		boolean deletefavourite = favouriteService.removeFavourite(favouriteDto);
+		HashMap<String, String> response = new HashMap<>();
+		if (deletefavourite) {
+			response.put("Message", "Course removed from favourites");
+			return new ResponseEntity<>(response, HttpStatus.OK);
+		} else {
+			response.put("Message", "Something went wrong");
+			return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+		}
 	}
-	
+
 }

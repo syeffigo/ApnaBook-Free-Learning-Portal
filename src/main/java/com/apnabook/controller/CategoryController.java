1fa746695c9db3ae956300f2208f6b306b1d1a40
syeffigo <158276441+syeffigo@users.noreply.github.com>
2024-02-15 16:49:35 +0530

    Update CategoryController.java

--- src/main/java/com/apnabook/controller/CategoryController.java
+++ src/main/java/com/apnabook/controller/CategoryController.java
@@ -1,8 +1,12 @@
 package com.apnabook.controller;
 
+import java.util.HashMap;
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,21 +15,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.apnabook.dto.CategoryDto;
+import com.apnabook.model.Category;
 import com.apnabook.service.CategoryService;
 
 @RestController
 @RequestMapping("/category")
 public class CategoryController {
-   @Autowired
-   private CategoryService categoryService;
-   
-   @PostMapping("/create")
-   public ResponseEntity<?> createCategory(@Valid @RequestBody CategoryDto categoryDto){
-	   return categoryService.addCategory(categoryDto);
-   }
-   
-   @GetMapping("/all")
-   public ResponseEntity<?> getAllCategory(){
-	   return categoryService.getCategory();
-   }
+	@Autowired
+	private CategoryService categoryService;
+
+	@PostMapping("/create")
+	public ResponseEntity<?> createCategory(@Valid @RequestBody CategoryDto categoryDto) {
+		boolean add = categoryService.addCategory(categoryDto);
+		HashMap<String, String> response = new HashMap<>();
+
+		if (add) {
+			response.put("Message", "Category Added.");
+			return new ResponseEntity<>(response, HttpStatus.CREATED);
+		} else {
+			response.put("Message", "Something went wrong");
+			return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+		}
+	}
+
+	@GetMapping("/all")
+	public ResponseEntity<?> getAllCategory() {
+		HashMap<String, List<Category>> response = new HashMap<>();
+		return new ResponseEntity<>(response, HttpStatus.ACCEPTED);
+	}
 }

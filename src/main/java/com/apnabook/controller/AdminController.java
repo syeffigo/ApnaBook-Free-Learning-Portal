43e0b372c9c25f30412db8ffa0688ae3c4d00d4c
syeffigo <158276441+syeffigo@users.noreply.github.com>
2024-02-15 16:47:52 +0530

    Update AdminController.java

--- src/main/java/com/apnabook/controller/AdminController.java
+++ src/main/java/com/apnabook/controller/AdminController.java
@@ -1,8 +1,11 @@
 package com.apnabook.controller;
 
+import java.util.HashMap;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,16 +19,34 @@ import com.apnabook.service.UserService;
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
-     @Autowired
-     private UserService userService;
-     
-     @PostMapping("/register")
-     public ResponseEntity<?> register(@Valid @RequestBody RegisterUserDto  registerUserDto){
-    	 return userService.registerUser(registerUserDto);
-     }
-     
-     @PostMapping("/login")
-     public ResponseEntity<?> login(@Valid @RequestBody LoginUserDto loginUserDto){
-    	 return userService.loginUser(loginUserDto);
-     }
+	@Autowired
+	private UserService userService;
+
+	@PostMapping("/register")
+	public ResponseEntity<?> register(@Valid @RequestBody RegisterUserDto registerUserDto) {
+		boolean register = userService.registerUser(registerUserDto);
+		HashMap<String, String> response = new HashMap<>();
+
+		if (register) {
+			response.put("Message", "User registered successfully.");
+			return new ResponseEntity<>(response, HttpStatus.CREATED);
+		} else {
+			response.put("Error", "User already exists");
+			return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+		}
+	}
+
+	@PostMapping("/login")
+	public ResponseEntity<?> login(@Valid @RequestBody LoginUserDto loginUserDto) {
+		boolean login = userService.loginUser(loginUserDto);
+		HashMap<String, String> response = new HashMap<>();
+
+		if (login) {
+			response.put("Message", "User logged in Successfully");
+			return new ResponseEntity<>(response, HttpStatus.ACCEPTED);
+		} else {
+			response.put("Message", "User authentication failed");
+			return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+		}
+	}
 }
